e0885344764c0c11ead10b6e0e8f26a84daf0260
aleksichen <aleksichen@gmail.com>
2024-08-12 09:39:50 +0800

    Update auth.rs

--- api/src/auth.rs
+++ api/src/auth.rs
@@ -2,7 +2,7 @@ use actix_web::{Error, get, HttpRequest, HttpResponse};
 use seajob_common::response::ApiResponse;
 
 // TODO: traefik中间件鉴权验证
-#[get("/")]
+#[get("/check")]
 async fn check(_req: HttpRequest) -> Result<HttpResponse, Error> {
     Ok(HttpResponse::Ok()
         .append_header(("X-User-Id", 1))
