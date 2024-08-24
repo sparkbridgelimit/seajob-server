971bcc69541f6f99fd92b0b46da022e7f1698664
aleksichen <aleksichen@gmail.com>
2024-08-24 19:35:22 +0800

    feat: add web entry

--- entry/mind/src/index.rs
+++ entry/mind/src/index.rs
@@ -1,18 +1,85 @@
-use actix_web::{get, Error, HttpMessage, HttpRequest, HttpResponse};
-use log::info;
-use seajob_common::response::ApiResponse;
-use seajob_dto::user_context::UserContext;
+use actix_web::{get, HttpResponse, HttpResponseBuilder, Responder};
+use actix_web::http::StatusCode;
+use serde::Serialize;
+
+// 定义通用的ApiResponse结构体
+#[derive(Serialize)]
+pub struct ApiResponse<T> {
+    success: bool,
+    data: Option<T>,
+    error_code: Option<u32>,
+    error_message: Option<String>,
+}
+
+// 实现 ApiResponse 的构造函数
+impl<T> ApiResponse<T> {
+    fn success(data: T) -> Self {
+        ApiResponse {
+            success: true,
+            data: Some(data),
+            error_code: None,
+            error_message: None,
+        }
+    }
+}
+
+// 定义 ApiResponder 类型来处理泛型返回
+pub struct ApiResponder<T>(pub T);
+
+// 为泛型 T 实现 Responder trait，使其自动包装到 ApiResponse 中
+// 为 ApiResponder<T> 实现 Responder trait
+impl<T> Responder for ApiResponder<T>
+where
+    T: Serialize,
+{
+    type Body = actix_web::body::BoxBody;
+
+    fn respond_to(self, _: &actix_web::HttpRequest) -> HttpResponse<Self::Body> {
+        let api_response = ApiResponse::success(self.0);
+        HttpResponseBuilder::new(StatusCode::OK).json(api_response)
+    }
+}
+
+// 为 Responder 提供泛型实现，使其自动转换为 ApiResponder
+impl<T> From<T> for ApiResponder<T> {
+    fn from(data: T) -> Self {
+        ApiResponder(data)
+    }
+}
+
+struct NewResponder<T>(T);
+
+// 为 NewResponder 实现 Responder，间接实现对泛型 T 的支持
+impl<T> Responder for NewResponder<T>
+where
+    T: Serialize,
+{
+    type Body = actix_web::body::BoxBody;
+
+    fn respond_to(self, req: &actix_web::HttpRequest) -> HttpResponse<Self::Body> {
+        ApiResponder::from(self.0).respond_to(req)
+    }
+}
+
+#[derive(Serialize)]
+struct Age {
+    value: i64,
+
+}
+
+#[derive(Serialize)]
+struct User {
+    name: String,
+    age: Age,
+}
+
 
 #[get("/")]
-async fn index(req: HttpRequest) -> Result<HttpResponse, Error> {
-    // 尝试从请求的 extensions 中获取 UserContext
-    if let Some(user_context) = req.extensions().get::<UserContext>() {
-        // 打印 user_id
-        info!("User ID: {:?}", user_context.user_id);
-    } else {
-        // 如果没有 UserContext
-        info!("No UserContext found");
-    }
-    let response = ApiResponse::success("hello man");
-    Ok(HttpResponse::Ok().json(response))
+async fn index() -> NewResponder<User> {
+    NewResponder(User {
+        name: "asd".to_string(),
+        age: Age {
+            value: 12
+        },
+    })
 }
--- entry/mind/src/router.rs
+++ entry/mind/src/router.rs
@@ -19,7 +19,7 @@ pub fn need_auth_routes(cfg: &mut web::ServiceConfig) {
 // 模块主入口
 pub fn entry(cfg: &mut web::ServiceConfig) {
     cfg.service(
-        web::scope("/mind/api")
+        web::scope("/api")
             .service(web::scope("/f").configure(not_auth_routes))
             .service(web::scope("/s").configure(need_auth_routes)),
     );
