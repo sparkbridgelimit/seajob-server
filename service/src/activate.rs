3e72db583cc542bc0b765b41c32a96de6e97b53e
aleksichen <aleksichen@gmail.com>
2024-09-09 23:24:50 +0800

    激活逻辑修复

--- service/src/activate.rs
+++ service/src/activate.rs
@@ -1,11 +1,93 @@
 use crate::err::ServiceError;
-use chrono::{Duration, Utc};
+use chrono::{DateTime, Duration, Utc};
 use sea_orm::ActiveValue::Set;
 use sea_orm::{ActiveModelTrait, EntityTrait, ColumnTrait, QueryFilter, TransactionTrait};
 use seajob_common::db;
 use seajob_common::id_gen::rand_numeric::rand_numeric;
 use seajob_dto::req::activate::{ConsumeActivateCodeReq, CreateActivateCodeReq};
 use seajob_entity::member::{activation_code, user_activation_log, user_membership};
+use seajob_entity::member::activation_code::ActiveModel;
+use seajob_entity::member::user_membership::Model;
+
+async fn update_activation_code(
+    code: &activation_code::Model,
+    txn: &sea_orm::DatabaseTransaction,
+) -> Result<ActiveModel, ServiceError> {
+    let mut code_active_model: ActiveModel = code.clone().into();
+    code_active_model.is_used = Set(true);
+    code_active_model
+        .save(txn)
+        .await
+        .map_err(ServiceError::DbError)
+}
+
+async fn create_activation_log(
+    user_id: i64,
+    code: &activation_code::Model,
+    expires_at: DateTime<Utc>,
+    txn: &sea_orm::DatabaseTransaction,
+) -> Result<user_activation_log::Model, ServiceError> {
+    user_activation_log::ActiveModel {
+        id: Default::default(),
+        user_id: Set(user_id),
+        activation_code_id: Set(code.id),
+        biz_code: Set(code.biz_code.clone()),
+        activated_at: Set(Utc::now()),
+        expires_at: Set(expires_at),
+        create_time: Default::default(),
+        update_time: Default::default(),
+    }
+        .insert(txn)
+        .await
+        .map_err(ServiceError::DbError)
+}
+
+async fn update_membership(
+    membership: &Model,
+    new_expires_at: DateTime<Utc>,
+    txn: &sea_orm::DatabaseTransaction,
+) -> Result<Model, ServiceError> {
+    let mut membership_active_model: user_membership::ActiveModel = membership.clone().into();
+    membership_active_model.expires_at = Set(new_expires_at);
+    membership_active_model.update_time = Set(Some(Utc::now()));
+    membership_active_model
+        .update(txn)
+        .await
+        .map_err(ServiceError::DbError)
+}
+
+async fn create_membership(
+    user_id: i64,
+    biz_code: String,
+    expires_at: DateTime<Utc>,
+    txn: &sea_orm::DatabaseTransaction,
+) -> Result<Model, ServiceError> {
+    user_membership::ActiveModel {
+        id: Default::default(),
+        user_id: Set(user_id),
+        biz_code: Set(biz_code),
+        expires_at: Set(expires_at),
+        create_time: Default::default(),
+        update_time: Default::default(),
+    }
+        .insert(txn)
+        .await
+        .map_err(ServiceError::DbError)
+}
+
+fn calculate_new_expiry(
+    membership: &Model,
+    duration_days: i64,
+) -> DateTime<Utc> {
+    let now = Utc::now();
+    if membership.expires_at < now {
+        // 会员已过期，从当前时间开始增加
+        now + Duration::days(duration_days)
+    } else {
+        // 会员未过期，从当前的失效时间增加
+        membership.expires_at + Duration::days(duration_days)
+    }
+}
 
 pub struct ActivateService;
 
@@ -13,7 +95,7 @@ impl ActivateService {
     pub async fn create(
         req: CreateActivateCodeReq,
     ) -> Result<activation_code::Model, ServiceError> {
-        activation_code::ActiveModel {
+        ActiveModel {
             id: Default::default(),
             code: Set(rand_numeric(8)),
             expire_at: Set(Utc::now() + Duration::days(30)),
@@ -49,52 +131,39 @@ impl ActivateService {
             }
         };
 
+        // 查询用户的会员资格记录
+        let existed_user_membership = user_membership::Entity::find()
+            .filter(user_membership::Column::UserId.eq(user_id))
+            .filter(user_membership::Column::BizCode.eq("seajob|user|try".to_string()))
+            .one(db::conn())
+            .await
+            .map_err(|e| ServiceError::DbError(e))?;
+
+        let now = Utc::now();
+        // 获取激活码有效期
+        let duration_days = code.duration;
+
         db::conn()
             .transaction::<_, _, ServiceError>(|txn| {
                 Box::pin(async move {
-                    // 更新is_used = true
-                    let mut code_active_model: activation_code::ActiveModel = code.into();
-                    code_active_model.is_used = Set(true);
+                    // 更新激活码状态
+                    update_activation_code(&code, txn).await?;
 
-                    // 提前提取所需的字段值
-                    let duration_value = code_active_model.duration.clone().unwrap();
-                    let biz_code_value = code_active_model.biz_code.clone().unwrap();
-                    let activation_code_id_value = code_active_model.id.clone().unwrap();
-
-                    code_active_model.save(txn).await?;
-
-                    // 会员生效时间
-                    let now = Utc::now();
-                    // 会员失效时间
-                    let expires_at = now + Duration::days(duration_value);
+                    // 会员的失效时间，基于当前时间或会员的现有到期时间
+                    let new_expires_at = match existed_user_membership {
+                        Some(ref membership) => calculate_new_expiry(membership, duration_days),
+                        None => now + Duration::days(duration_days),
+                    };
 
                     // 创建激活记录
-                    user_activation_log::ActiveModel {
-                        id: Default::default(),
-                        user_id: Set(user_id),
-                        activation_code_id: Set(activation_code_id_value),
-                        biz_code: Set(biz_code_value.clone()), // 使用解包后的值
-                        activated_at: Set(now),
-                        expires_at: Set(expires_at),
-                        create_time: Default::default(),
-                        update_time: Default::default(),
-                    }
-                        .insert(txn)
-                        .await
-                        .map_err(|e| ServiceError::DbError(e))?;
+                    create_activation_log(user_id, &code, new_expires_at, txn).await?;
 
-                    // 创建会员资格记录
-                    user_membership::ActiveModel {
-                        id: Default::default(),
-                        user_id: Set(user_id),
-                        biz_code: Set(biz_code_value), // 使用解包后的值
-                        expires_at: Set(expires_at),
-                        create_time: Default::default(),
-                        update_time: Default::default(),
+                    // 更新或创建会员资格
+                    if let Some(membership) = existed_user_membership {
+                        update_membership(&membership, new_expires_at, txn).await?;
+                    } else {
+                        create_membership(user_id, code.biz_code, new_expires_at, txn).await?;
                     }
-                        .insert(txn)
-                        .await
-                        .map_err(|e| ServiceError::DbError(e))?;
 
                     Ok(true)
                 })
